d45c771fa50310fa0358859e50cf3ddd4b810691
Winicius <78980622+winisc@users.noreply.github.com>
2024-08-29 15:27:48 -0300

    BackEndFinalizado

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,9 +1,82 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
+app.use(express.json())
 
-app.get('/vendas', (req, res) => {
-    res.send('Ok')
+
+app.post('/vendas', async (req, res) => {
+    
+    await prisma.user.create({
+        data: {
+            produto: req.body.produto, 
+            quantidade:  req.body.quantidade,
+            valor: req.body.valor,
+            tipo:      req.body.tipo,
+            valorTotal: req.body.valorTotal,
+            valorPago: req.body.valorPago,
+            troco:  req.body.troco
+        }
+    })
+    
+    res.status(201).json(req.body)
+})
+
+app.get('/vendas', async (req, res) => {
+    
+    let vendas = []
+
+    if(req.query){
+        vendas = await prisma.user.findMany({
+            where:{
+                produto: req.query.produto, 
+                quantidade:  req.query.quantidade,
+                valor: req.query.valor,
+                tipo:      req.query.tipo,
+                valorTotal: req.query.valorTotal,
+                valorPago: req.query.valorPago,
+                troco:  req.query.troco
+            }
+        })
+    }
+    else{
+        vendas = await prisma.user.findMany()
+    }
+
+    res.status(200).json(vendas)
+})
+
+app.put('/vendas/:id', async (req, res) => {
+    
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            produto: req.body.produto, 
+            quantidade:  req.body.quantidade,
+            valor: req.body.valor,
+            tipo:      req.body.tipo,
+            valorTotal: req.body.valorTotal,
+            valorPago: req.body.valorPago,
+            troco:  req.body.troco
+        }
+    })
+    
+    res.status(201).json(req.body)
+})
+
+app.delete('/vendas/:id', async (req, res) =>{
+    
+    await prisma.user.delete({
+        where:{
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({message: "Venda deletada com sucesso!"})
 })
 
 app.listen(3000)
@@ -12,6 +85,18 @@ app.listen(3000)
 /*
     Criar API de Vendas
 
-    -
+    {
+        "produtos": ["manicoto","parafuso"],
+        "quantidades":[1,2],
+        "valores": [10,4],
+        "tipos": ["produto","produto"],
+        "valorTotal": 14,
+        "tipoDePagamento": "Cartao de Credito",
+        "valorPago": 14,
+        "troco": "Sem troco"
+    }
+
+    winisc
+    WTysVG3V6cC9Di0C
 
 */
