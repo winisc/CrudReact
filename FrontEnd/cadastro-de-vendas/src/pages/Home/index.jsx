ad50cb31eb2fabbd7ad706acfdf333ce868f47d5
Winicius <78980622+winisc@users.noreply.github.com>
2024-08-31 16:47:49 -0300

    FrontEndAtt02

--- FrontEnd/cadastro-de-vendas/src/pages/Home/index.jsx
+++ FrontEnd/cadastro-de-vendas/src/pages/Home/index.jsx
@@ -1,69 +1,110 @@
-import './style.css'
+import { useRef } from "react";
+import "./style.css";
 
 function App() {
+  const inputProduto = useRef()
+  const inputQuantidade = useRef()
+  const inputValor = useRef()
+  const inputServico = useRef()
+
+  let listaDeProdutos = []
+
+  function createProduto(){
+    const produto = {
+      name: inputProduto.current.value,
+      quantidade: inputQuantidade.current.value,
+      valor: inputValor.current.value,
+      servico: inputServico.current.value
+    }
+
+    for(let i = 0; i < 10; i++)
+    {
+      if(listaDeProdutos[i] === undefined)
+      {
+        listaDeProdutos[i] = produto
+        return
+      }
+      else{      
+      }
+    }
+  }
 
   return (
+    <div className="principal" >
 
-    <div className="container">
-      <form>
-        <text>Produto:</text>
-        <input name="" type="text"/>
-        <text>Quantidade:</text>
-        <input name="" type="number"/>
-        <text>Valor:</text>
-        <input name="" type="number"/>
-        <text>Tipo de Produto:</text>
-        <select class="value-tipo" id="tipo-de-servico0" name="tipo" required="required">
-          <option value="">Escolha o tipo</option>
-          <option value="servico">Serviços</option>
-          <option value="pecas">Peças</option>
-          <option value="outros">Outros</option>
-      </select>
-        <button type="button">Adicionar Produto</button>
-      </form>
+      <div className="container">
+        <form>
+          <text>Produto:</text>
+          <input name="name" type="text" ref={inputProduto  } />
+          <text>Quantidade:</text>
+          <input name="qt" type="number" ref={inputQuantidade}/>
+          <text>Valor:</text>
+          <input name="valor" type="number" ref={inputValor}/>
+          <text>Tipo de Produto:</text>
+          <select
+            class="value-tipo"
+            id="tipo-de-servico"
+            name="tipo"
+            required="required"
 
-      <form className="form-lista-de-produtos">
-        <text className="quantidade">QT</text>
-        <text className="produto">Produto</text>
-        <text className="valor">Valor</text>
+            ref={inputServico}
+          >
+            <option value="">Escolha o tipo de Produto</option>
+            <option value="servicos">Serviços</option>
+            <option value="pecas">Peças</option>
+            <option value="outros">Outros</option>
+          </select>
+          <button type="button" onClick={createProduto}>Adicionar Produto</button>
+        </form>
 
-        <div className="lista-de-produtos">
-          <div className="quantidade-lista">
-            <p>01</p>
-            <p>01</p>
-            <p>01</p>
-            <p>02</p>
-            <p>01</p>
+        <form>
+          <div className="cabeçalho">
+            <text className="quantidade">QT</text>
+            <text className="produto">Produto</text>
+            <text className="valor">Valor</text>
           </div>
-          <div className="produto-lista">
-            <p>Camara de Ar</p>
-            <p>Cabo de Freio</p>
-            <p>Oleo Mobil</p>
-            <p>Parafuso</p>
-            <p>Mão de Obra</p>
-          </div>
-          <div className="valor-lista">
-            <p>R$40,00</p>
-            <p>R$25,00</p>
-            <p>R$35,00</p>
-            <p>R$6,00</p>
-            <p>R$15,00</p>
-          </div>
-        </div>
-      </form>
 
-      <form>
-        <h1>Vender</h1>
-        <input name="" type="text"/>
-        <input name="" type="text"/>
-        <input name="" type="text"/>
-        <input name="" type="text"/>
-        <button className="button-confirmar" type="button">Confirmar Venda</button>
-      </form>
+          <div className="lista-de-produtos">
+            <p className="quantidade-lista">01</p>
+            <p className="produto-lista">
+              Produto Produto Produto Produto Produto
+            </p>
+            <p className="valor-lista">R$40,00</p>
+          </div>
+        </form>
+
+        <form className="venda">
+          <h1>Valor Total:</h1>
+          <text className="valorPago">R$156,00</text>
+
+          <h1>Tipo de pagamento:</h1>
+          <select
+            class="value-tipo"
+            id="forma-de-pagamento"
+            name="forma"
+            required="required"
+          >
+            <option value="">Escolha a forma de Pagamento</option>
+            <option value="dinheiro">Dinheiro</option>
+            <option value="pix">Pix</option>
+            <option value="cardCredit">Cartão de Crédito</option>
+            <option value="cardDebito">Cartão de Débito</option>
+            <option value="pendente">Pendente</option>
+          </select>
+
+          <h1>Valor Pago:</h1>
+          <input name="" type="number" />
+
+          <h1>Troco:</h1>
+          <text className="troco">R$20,00</text>
+
+          <button className="button-confirmar" type="button">
+            Confirmar Venda
+          </button>
+        </form>
+      </div>
     </div>
-
-    
-  )
+  );
 }
 
-export default App
+export default App;
